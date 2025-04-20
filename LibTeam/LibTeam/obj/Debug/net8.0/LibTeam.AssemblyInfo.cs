e3b8a03cda4c0a9e1e81f9f46cdd0eda8134cf9a
DevDatNguyen02 <thathaivuong@gmail.com>
2025-04-20 15:26:54 +0700

    update-DocGiaController

--- LibTeam/LibTeam/obj/Debug/net8.0/LibTeam.AssemblyInfo.cs
+++ LibTeam/LibTeam/obj/Debug/net8.0/LibTeam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibTeam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6473d3cf6b46cc11fb6afee66ff354410a5e06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32190272624b982ded0bff3e8246d9feb8bc9b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibTeam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibTeam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
