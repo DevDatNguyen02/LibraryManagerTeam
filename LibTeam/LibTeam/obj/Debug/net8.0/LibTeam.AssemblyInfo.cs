a637e5b291444b7c464740a84bc292d11330c293
Lam <darkmagician202@gmail.com>
2025-05-08 08:18:43 +0700

    update view

--- LibTeam/LibTeam/obj/Debug/net8.0/LibTeam.AssemblyInfo.cs
+++ LibTeam/LibTeam/obj/Debug/net8.0/LibTeam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibTeam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3b8a03cda4c0a9e1e81f9f46cdd0eda8134cf9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86207f5dc6f6bd7a633abb4869fd762efca0eb15")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibTeam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibTeam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
